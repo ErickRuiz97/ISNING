d5d0ffa92bd9c28497ae377323cf55b8a5d29ba7
ErickRuiz97 <33972753+ErickRuiz97@users.noreply.github.com>
2025-02-13 14:58:41 -0600

    Modificando el header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,11 +1,22 @@
 <script setup>
 const urlLogo = "https://le-cdn.hibuwebsites.com/be04dc16d67f484bbb4edb2b6e8fdff9/dms3rep/multi/opt/vintage-outdoor-inc-logo-300w.png";
+
+const navItems = [
+  { name: "Home", link: "#" },
+  { name: "About Us", link: "#" },
+  { name: "Services", link: "#" },
+  { name: "Vintage Construction", link: "#" },
+  { name: "Gallery", link: "#" },
+  { name: "Testimonials", link: "#" },
+  { name: "Blog", link: "#" },
+  { name: "FAQS", link: "#" },
+];
 </script>
 
 <template>
   <header>
     <nav class="navbar navbar-expand-lg navbar-light">
-      <div class="container-fluid">
+      <div class="container-fluid d-flex justify-content-between align-items-center">
         <a class="navbar-brand d-flex align-items-center" href="#">
           <img :src="urlLogo" alt="Logo" width="80" height="50" class="d-inline-block align-text-top">
         </a>
@@ -15,30 +26,9 @@ const urlLogo = "https://le-cdn.hibuwebsites.com/be04dc16d67f484bbb4edb2b6e8fdff
         </button>
 
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav mx-auto">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About Us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Services</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Vintage Construction</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Gallery</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Testimonials</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">FAQS</a>
+          <ul class="navbar-nav me-auto">
+            <li v-for="(item, index) in navItems" :key="index" class="nav-item">
+              <a class="nav-link" :href="item.link">{{ item.name }}</a>
             </li>
             <li class="nav-item">
               <button type="button" class="btn btn-warning text-white">Contact Us</button>
@@ -49,6 +39,7 @@ const urlLogo = "https://le-cdn.hibuwebsites.com/be04dc16d67f484bbb4edb2b6e8fdff
     </nav>
   </header>
 </template>
+
 
 <style>
 .btn-warning {
